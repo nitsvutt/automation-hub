33cb14d9f530100dc72fd476aed0312cfa123980
nitsvutt <nitsvutt@gmail.com>
2024-08-19 22:32:11 +0700

    init project

---
+++ app/core/config.py
@@ -0,0 +1,12 @@
+import os
+
+# postgresql connection
+PRODUCT1_SERVER = os.environ['PRODUCT1_SERVER']
+PRODUCT1_PORT = os.environ['PRODUCT1_PORT']
+PRODUCT1_USER = os.environ['PRODUCT1_USER']
+PRODUCT1_PASSWORD = os.environ['PRODUCT1_PASSWORD']
+
+# datetime format
+TIMEZONE = 'Asia/Ho_Chi_Minh'
+DATE_FORMAT = '%Y-%m-%d'
+DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
