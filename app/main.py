17ed9e0a77cad88b1156ff3d07b8b3885e78c32e
nitsvutt <nitsvutt@gmail.com>
2024-08-19 23:34:31 +0700

    test navigation

--- app/main.py
+++ app/main.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
-st.title('Automation Hub')
+home = st.Page("pages/home.py", title="Home", icon=":material/home:")
+dashboard = st.Page("pages/dashboard.py", title="Dashboard", icon=":material/dashboard:")
+pipeline = st.Page("pages/pipeline.py", title="Pipeline", icon=":material/water_pump:")
+
+pg = st.navigation([home, dashboard, pipeline])
+
+pg.run()
