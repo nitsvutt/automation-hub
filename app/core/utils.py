1020b9695ee9988e757fcfd9710fd19a03ea4e65
nitsvutt <nitsvutt@gmail.com>
2024-08-24 18:22:10 +0700

    beautify

--- app/core/utils.py
+++ app/core/utils.py
@@ -6,13 +6,13 @@ from core.config import env, path
 
 class Page():
     def __init__(self):
-        self.home = st.Page(path.PAGE_PATH+"home.py", title="Home", icon=":material/home:")
-        self.dashboard = st.Page(path.PAGE_PATH+"dashboard.py", title="Dashboard", icon=":material/dashboard:")
-        self.pipeline = st.Page(path.PAGE_PATH+"pipeline.py", title="Pipeline", icon=":material/water_pump:")
-        self.documentation = st.Page(path.PAGE_PATH+"documentation.py", title="Documentation", icon=":material/description:")
+        self.home = st.Page(path.PAGE_PATH + "home.py", title="Home", icon=":material/home:")
+        self.dashboard = st.Page(path.PAGE_PATH + "dashboard.py", title="Dashboard", icon=":material/dashboard:")
+        self.pipeline = st.Page(path.PAGE_PATH + "pipeline.py", title="Pipeline", icon=":material/water_pump:")
+        self.documentation = st.Page(path.PAGE_PATH + "documentation.py", title="Documentation", icon=":material/description:")
 
 def make_style(css_file: str):
-    with open(path.CSS_PATH+css_file) as file:
+    with open(path.CSS_PATH + css_file) as file:
         css = file.read()
         st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
